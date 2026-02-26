c6933690826f7759cd01365fdffe59f473f5210d
Broninn <bruno-hs@outlook.com>
2026-02-26 13:31:56 -0300

    Updating Delivery Status

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { DeliveriesController } from '@/controllers/deliveries-controller';
+import { DeliveriesStatusController } from '@/controllers/deliveries-status-controller';
 
 import { ensureAuthenticated } from '@/middlewares/ensure-authenticated';
 
@@ -8,9 +9,12 @@ import { verifyUserAuth } from '@/middlewares/verifyUserAuth';
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuth(['sale']));
 deliveriesRoutes.post('/', deliveriesController.create);
 deliveriesRoutes.get('/', deliveriesController.index);
+
+deliveriesRoutes.patch('/:id/status', deliveriesStatusController.update);
 
 export { deliveriesRoutes };
