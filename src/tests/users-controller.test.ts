6c3a9329fe5561f9fbacca7c2a5490658111b9d7
Broninn <bruno-hs@outlook.com>
2026-03-13 13:05:07 -0300

    Removing Test User

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest';
 import { app } from '@/app';
+import { prisma } from '@/database/prisma';
 
 describe('UsersController', () => {
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: { id: user_id },
+    });
+  });
+
   it('should create a new user successfully', async () => {
     const response = await request(app).post('/users').send({
       name: 'John Doe',
@@ -12,5 +21,7 @@ describe('UsersController', () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
     expect(response.body.name).toBe('John Doe');
+
+    user_id = response.body.id;
   });
 });
